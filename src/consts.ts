a2bff510ce43abe5944d7bc9367a1f6a798b75ad
cpprhtn <cpprhtn@naver.com>
2023-09-20 01:07:22 +0900

    Refactor: SEO 포함 일부코드 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SUSC';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SUSC | 대학방학교류';
+export const SITE_DESCRIPTION = 'SUSC는 대학 IT 동아리 연합으로, 방학기간에 세미나, 멘토링, 프로젝트, 연구, 공모전 등 다양한 활동을 진행합니다.';

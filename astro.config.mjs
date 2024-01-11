fcfea2571a11be4a8135e7c3f0879b8e7329738b
choo121600 <choo121600@gmail.com>
2024-01-11 13:52:10 +0900

    Revert "Fix: hotfix"
    This reverts commit 99f88f0ac9f9f25d5caf0e3fa26de225bf62a52a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercelServerless(),
-  site: "https://example.com",
+  site: "https://susc.kr",
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
